1bbe2ea76b2aa60bca43baf192d4fe6e1463407f
Andre Pham <andrepham33@gmail.com>
2023-06-10 12:53:07 +1000

    App init

---
+++ Lemon/Lemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lemon
+//
+//  Created by Andre Pham on 10/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lemon/LemonUITests/LemonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LemonUITestsLaunchTests.swift
+//  LemonUITests
+//
+//  Created by Andre Pham on 10/6/2023.
+//
+
+import XCTest
+
+final class LemonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
