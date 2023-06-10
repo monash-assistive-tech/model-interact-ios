c5894cf213666d3c12dc3bc57c3d35fa221ea398
Andre Pham <andrepham33@gmail.com>
2023-06-11 07:27:33 +1000

    Created SpeechSynthesiser

---
+++ Lemon/Lemon/Model Layer/TTS/SpeechSynthesizer.swift
@@ -0,0 +1,75 @@
+//
+//  SpeechSynthesizer.swift
+//  Lemon
+//
+//  Created by Andre Pham on 11/6/2023.
+//
+
+import Foundation
+import AVFoundation
+
+class SpeechSynthesiser: NSObject, AVSpeechSynthesizerDelegate {
+    
+    enum Accent: String {
+        case american = "en-US"
+        case australian = "en-AU"
+        // Add more if desired, e.g. en-GB, en-IE
+    }
+    
+    private let synthesiser = AVSpeechSynthesizer()
+    private var rate: Float = 0.55
+    private var pitchMultiplier: Float = 0.9
+    private var postUtteranceDelay: TimeInterval = 0.0
+    private var volume: Float = 1.0
+    private var voice: AVSpeechSynthesisVoice
+    private var lastSpoken: String? = nil
+    
+    override init() {
+        self.voice = AVSpeechSynthesisVoice(language: Accent.american.rawValue)!
+        super.init()
+        self.synthesiser.delegate = self
+    }
+    
+    private func buildUtterance(speech: String) -> AVSpeechUtterance {
+        let utterance = AVSpeechUtterance(string: speech)
+        utterance.rate = self.rate
+        utterance.pitchMultiplier = self.pitchMultiplier
+        utterance.postUtteranceDelay = self.postUtteranceDelay
+        utterance.volume = self.volume
+        utterance.voice = self.voice
+        return utterance
+    }
+    
+    func speak(_ speech: String) {
+        let utterance = self.buildUtterance(speech: speech)
+        self.synthesiser.speak(utterance)
+    }
+    
+    func stopSpeaking() {
+        self.synthesiser.stopSpeaking(at: .immediate)
+    }
+    
+    func repeatLastSpoken() {
+        if let lastSpoken {
+            self.speak(lastSpoken)
+        }
+    }
+    
+}
+extension SpeechSynthesiser {
+    
+    // MARK: - AVSpeechSynthesizerDelegate
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) { }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) { }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) { }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) { }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) { }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) { }
+    
+}
--- Lemon/Lemon/View Controller Layer/ViewController.swift
+++ Lemon/Lemon/View Controller Layer/ViewController.swift
@@ -6,26 +6,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    private let synthesiser = SpeechSynthesiser()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        // Create a UIStackView
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
-        // Create a UILabel
-        let label = UILabel()
-        label.text = "Hello World"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
+        // Add padding to the stack view using layout margins
+        stackView.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        stackView.isLayoutMarginsRelativeArrangement = true
         
-        // Add the label to the view
-        view.addSubview(label)
+        // Create a header label
+        let headerLabel = UILabel()
+        headerLabel.text = "Hello World"
+        headerLabel.font = UIFont.boldSystemFont(ofSize: 24)
         
-        // Set label constraints to center it in the view
+        // Create a button
+        let button = UIButton(type: .system)
+        button.setTitle("Hello Button", for: .normal)
+        button.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
+        
+        // Add the header label and button to the stack view
+        stackView.addArrangedSubview(headerLabel)
+        stackView.addArrangedSubview(button)
+        
+        // Add the stack view to the view
+        view.addSubview(stackView)
+        
+        // Set stack view constraints to take up the entire screen
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+        
+//        // Add border to the stack view
+//        button.layer.borderWidth = 1.0
+//        button.layer.borderColor = UIColor.black.cgColor
+//        // Add border to the stack view
+//        headerLabel.layer.borderWidth = 1.0
+//        headerLabel.layer.borderColor = UIColor.black.cgColor
+        
+        let spacerView = UIView()
+        spacerView.translatesAutoresizingMaskIntoConstraints = false
+        spacerView.setContentHuggingPriority(.defaultLow, for: .vertical)
+        spacerView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        stackView.addArrangedSubview(spacerView)
+    }
+    
+    @objc private func buttonTapped() {
+        self.synthesiser.speak("Andre is very cool!")
     }
 
 }
