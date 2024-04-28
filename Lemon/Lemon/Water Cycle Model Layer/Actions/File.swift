11be58e790788815fdb4ff56565da6a1a13e3b71
ishwiz <154478861+ishwiz@users.noreply.github.com>
2024-04-28 23:55:20 +1000

    integrated mlmodel file and created actions enum for water cycle model

---
+++ Lemon/Lemon/Water Cycle Model Layer/Actions/File.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  Lemon
+//
+//  Created by Ishrat Kaur on 28/4/2024.
+//
+
+import Foundation
+
+enum WaterCycleAudioAction {
+    
+    case cloud
+    case condensation
+    case evaporation
+    case GroundWater
+    case Infiltration
+    case Mountain
+    case Ocean
+    case PlantUptake
+    case Precipitation
+    case Rain
+    case River
+    case RunOff
+    case Snow
+    case SunMelt
+    case Sun
+    case Transpiration
+    case Arrow
+    // discuss the following with team
+    case completed
+    case correct
+    case none
+    
+}
